14122713dd9d753a45a50789f5820f86ea8b0ce8
temuri pkhaladze <kverna133@gmail.com>
2016-03-31 03:53:52 +0400

    ubgrade model

--- application/controllers/Service.php
+++ application/controllers/Service.php
@@ -4,30 +4,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Service extends CI_Controller {
 	public function __construct() {
         parent::__construct();
-        $this->load->model('Model_Service');
+        $this->load->model('model_service');
     }
 	public function ShortNews(){
-		echo json_encode($this->Model_Service->ShortNews());
+		echo json_encode($this->model_service->ShortNews());
 	}
 	public function FullNews(){
-		echo json_encode($this->Model_Service->FullNews());
+		echo json_encode($this->model_service->FullNews());
 	}
 	public function ShortTeam(){
-		echo json_encode($this->Model_Service->ShortTeam());
+		echo json_encode($this->model_service->ShortTeam());
 	}
 	public function FullTeam(){
-		echo json_encode($this->Model_Service->FullTeam());
+		echo json_encode($this->model_service->FullTeam());
 	}
 	public function ShortProjects(){
-		echo json_encode($this->Model_Service->ShortProjects());
+		echo json_encode($this->model_service->ShortProjects());
 	}
 	public function FullProjects(){
-		echo json_encode($this->Model_Service->FullProjects());
+		echo json_encode($this->model_service->FullProjects());
 	}
 	public function Partners(){
-		echo json_encode($this->Model_Service->Partners());
+		echo json_encode($this->model_service->Partners());
 	}
 	public function Donors(){
-		echo json_encode($this->Model_Service->Donors());
+		echo json_encode($this->model_service->Donors());
 	}
 }
--- application/models/Model_Service.php
+++ application/models/Model_Service.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Model_Service extends CI_Model {
 	public function ShortNews(){
-		$language = $_GET['language'];
+		$language = (isset($_POST['language']))?$_POST['language']:$_GET['language'];
 		$this->db->select('phr_content.id, phr_content.image, phr_content.create_time, phr_content.short_desc_'.$language.', phr_content.title_'.$language);
 		$this->db->join('phr_menu_content','phr_menu_content.content_id=phr_content.id');
 		$this->db->where('phr_menu_content.menu_id',40);
@@ -15,7 +15,7 @@ class Model_Service extends CI_Model {
 		return $query;
 	}
 	public function FullNews(){
-		$language = $_GET['language'];
+		$language = (isset($_POST['language']))?$_POST['language']:$_GET['language'];
 		$id =$this->input->get('id');
 		$this->db->select('id, image, create_time, desc_'.$language.', title_'.$language);
 		$this->db->where('id', $id);
@@ -27,7 +27,7 @@ class Model_Service extends CI_Model {
 		return $query;
 	}
 	public function ShortTeam(){
-		$language = $_GET['language'];
+		$language = (isset($_POST['language']))?$_POST['language']:$_GET['language'];
 		$this->db->select('phr_content.id, phr_content.image, phr_content.create_time, phr_content.short_desc_'.$language.', phr_content.title_'.$language);
 		$this->db->join('phr_menu_content','phr_menu_content.content_id=phr_content.id');
 		$this->db->where('phr_menu_content.menu_id',42);
@@ -39,7 +39,7 @@ class Model_Service extends CI_Model {
 		return $query;
 	}
 	public function FullTeam(){
-		$language = $_GET['language'];
+		$language = (isset($_POST['language']))?$_POST['language']:$_GET['language'];
 		$id = $_GET['id'];
 		$this->db->select('id, image, create_time, desc_'.$language.', title_'.$language);
 		$this->db->where('id', $id);
@@ -51,7 +51,7 @@ class Model_Service extends CI_Model {
 		return $query;
 	}
 	public function ShortProjects(){
-		$language = $_GET['language'];
+		$language = (isset($_POST['language']))?$_POST['language']:$_GET['language'];
 		$this->db->select('phr_content.id, phr_content.image, phr_content.create_time, phr_content.short_desc_'.$language.', phr_content.title_'.$language);
 		$this->db->join('phr_menu_content','phr_menu_content.content_id=phr_content.id');
 		$this->db->where('phr_menu_content.menu_id',47);
@@ -63,7 +63,7 @@ class Model_Service extends CI_Model {
 		return $query;
 	}
 	public function FullProjects(){
-		$language = $_GET['language'];
+		$language = (isset($_POST['language']))?$_POST['language']:$_GET['language'];
 		$id = $_GET['id'];
 		$this->db->select('id, image, create_time, desc_'.$language.', title_'.$language);
 		$this->db->where('id', $id);
@@ -128,14 +128,4 @@ class Model_Service extends CI_Model {
 			);
 		return $Partners;
 	}
-// 	arra(
-//  array(
-//     ['img']=>'img.jpg',
-//     ['url']=>http://example.com
-//   ),
-//   array(
-//     ['img']=>'img.jpg',
-//     ['url']=>http://example.com
-//   )
-// )
 }
